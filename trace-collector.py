40faef19c7ccb71ccdeaeb7e2bc0531efa687240
Ridge Kennedy <ridge.kennedy@alliedtelesis.co.nz>
2012-11-29 15:25:55 +1300

    Seperate files per package

--- trace-collector.py
+++ trace-collector.py
@@ -3,11 +3,12 @@ import argparse
 import json
 import fnmatch
 import os
+import time
 import threading
 import SocketServer
 import sys
 import shlex
-from Queue import Queue
+import Queue
 
 
 class TraceHandler(SocketServer.StreamRequestHandler):
@@ -27,7 +28,7 @@ class TraceHandler(SocketServer.StreamRequestHandler):
         try:
             package, cwd, cmd = data.split('\t')
             c_file = self.get_file(cmd)
-            if c_file and ('conftest.c' not in c_file):
+            if c_file and ('conftest.' not in c_file):
                 self.server.queue.put(
                     (package, {'command': cmd,
                                'directory': cwd,
@@ -36,19 +37,59 @@ class TraceHandler(SocketServer.StreamRequestHandler):
         except:
             print stderr, 'Received malformed trace "%s"' % data
 
+class CompileCommandStorage(object):
+    def __init__(self, path):
+        if not os.access(path, os.W_OK):
+            try:
+                os.mkdir(path)
+            except IOError:
+                raise IOError('Output directory is not writable %s' % path)
+        self.path = path
+
+        self.handles = {}
+
+    def _create_package_file(self, package):
+        if not self.handles.has_key(package):
+            f = open(os.path.join(self.path, 'compile_commands.%s.json' % package), 'w')
+            f.write('[')
+            self.handles[package] = f
+
+    def _close_package_files(self):
+        for f in self.handles.itervalues():
+            f.write(']')
+            f.close()
+
+    def close(self):
+        self._close_package_files()
+
+    def store(self, package, command):
+        if not self.handles.has_key(package):
+            self._create_package_file(package)
+        f = self.handles[package]
+        if f.tell() != 1:
+            f.write(',')
+        f.write(json.dumps(command))
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Trace Collector')
-    parser.add_argument('--host', default='localhost')
-    parser.add_argument('--port', type=int, default=44444)
-    parser.add_argument('--unix')
+ 
+    parser.add_argument(
+        'port', type=int, help='Port to bind to'
+    )
+    parser.add_argument(
+        '--addr', default='localhost', help='Address to bind to'
+    )
+    parser.add_argument(
+        '-o', nargs=1, required=True, metavar='PATH', help='Output directory'
+    )
     args = parser.parse_args()
 
-    HOST = args.host
+    HOST = args.addr
     PORT = args.port
+    O_PATH = args.o[0]
 
-
-    q = Queue()
+    q = Queue.Queue()
     s = SocketServer.TCPServer((HOST, PORT), TraceHandler)
     s.queue = q
     server_thread = threading.Thread(target=s.serve_forever)
@@ -56,13 +97,18 @@ if __name__ == '__main__':
     server_thread.start()
     print 'Listening on %s:%i' % (HOST, PORT)
 
+    store = CompileCommandStorage(O_PATH)
+
     try:
         while 1:
-            package, trace = q.get()
-            print trace
-            q.task_done()
+            try:
+                store.store(*q.get(block=False))
+                q.task_done()
+            except Queue.Empty:
+                time.sleep(0.5)
     except KeyboardInterrupt:
         pass
     finally:
+        store.close()
         s.shutdown()
 
